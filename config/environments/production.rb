1297b9a6eb94b5ea2367618f241b64882c7114ee
John Lester Nuccio <nuccio.john@gmail.com>
2014-06-01 16:30:04 -0700

    Add Amazon S3 for Heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,4 +83,14 @@ Rails.application.configure do
 
    # Needed for Devise and Heroku
   config.action_mailer.default_url_options = { :host => 'http://purrrfectmatch.herokuapp.com/' }
+
+  # This sets paperclip to upload amazon s3, needed for heroku
+  config.paperclip_defaults = {
+    :storage => :s3,
+    :s3_credentials => {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
