2125d3faaa7e6566146d83773e4a43f7f718a5ab
John Lester Nuccio <nuccio.john@gmail.com>
2014-06-01 21:17:39 -0700

    added validations for profile fields

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,4 +4,11 @@ class Profile < ActiveRecord::Base
      has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }
      validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png"]
 
+    validates :image, presence: true
+    validates :name, presence: true
+  	validates :about, presence: true
+  	validates :personality, presence: true
+  	validates :breed, presence: true
+  	validates :religion, presence: true
+  	validates :interests, presence: true
 end
